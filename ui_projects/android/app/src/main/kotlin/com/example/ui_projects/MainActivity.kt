e404fd3dd5435e4571416b6b5f845db1ae0149d1
souroveskb <souroveskb@gmail.com>
2022-04-23 11:41:08 +0600

    updated

--- ui_projects/android/app/src/main/kotlin/com/example/ui_projects/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ui_projects
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ui_projects/lib/animation/parallax/parallax_animation.dart
+++
@@ -1,4 +0,0 @@
-library parallax_animation;
-
-export 'parallax_area.dart';
-export 'parallax_widget.dart';
